198df562ca0eba207e1690be5a050c4b5b6ecdbd
andrei110 <andrei_vaida98@yahoo.com>
2025-05-19 22:51:12 +0300

    feat: current location was added on map

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ const countryContainer = document.querySelector('.container__country');
 const neighboursContainer = document.querySelector('.neighbours__countries');
 const neighboursContainerBig = document.querySelector('.container__neighbours');
 const containerInputs = document.querySelector('.container__inputs');
+const containerLocation = document.querySelector('.container__location');
 
 // Media Querries
+const query800 = window.matchMedia('(max-width: 800px)');
 const query1000Up = window.matchMedia('(min-width: 1000px)');
 const query1000Down = window.matchMedia('(max-width: 1000px)');
 
@@ -20,7 +22,9 @@ function formatNumber(num) {
 
 function renderCountry(data, container, className = '', dataCity) {
   const html = `
-  <article class="country ${className}">
+  <article class="country ${className} ${
+    dataCity || data.name === inputSearch.value ? `country__main` : ''
+  }">
     <img class="country__img" src="${data.flags.svg}" />
     <div class="country__data">
       <h3 class="country__name">${data.name}</h3>
@@ -48,8 +52,10 @@ function renderCountry(data, container, className = '', dataCity) {
       `<h1 class="neighbours__title">The Neighbours of ${data.name}</h1>`
     );
 
-  container.insertAdjacentHTML('beforeend', html);
-  container.style.opacity = 1;
+  if (dataCity || data.name === inputSearch.value)
+    containerLocation.insertAdjacentHTML('beforeend', html);
+  if (!dataCity && data.name !== inputSearch.value)
+    container.insertAdjacentHTML('beforeend', html);
 }
 
 function getPosition() {
@@ -109,8 +115,8 @@ function moveBtns(direction, height, width) {
 }
 
 function resetUI() {
-  if (document.querySelector('.country'))
-    countryContainer.lastChild.style.display = 'none';
+  containerLocation.classList.remove('hidden');
+  if (containerLocation) containerLocation.innerHTML = '';
   if (neighboursContainer) neighboursContainer.innerHTML = '';
   if (document.querySelector('.neighbours__title'))
     document.querySelector('.neighbours__title').style.display = 'none';
@@ -120,8 +126,59 @@ function resetUI() {
   }
   if (query1000Down.matches) moveBtns('collumn', '40vh', 'inherit');
   neighboursContainerBig.classList.remove('hidden');
+  containerLocation.style.height = 'inherit';
 }
 
+function renderMap(lat, lng) {
+  const html = `<div id="map"></div>`;
+  containerLocation.insertAdjacentHTML('beforeend', html);
+  const map = new maplibregl.Map({
+    style: 'https://tiles.openfreemap.org/styles/liberty',
+    center: [lng, lat],
+    zoom: 13,
+    container: 'map',
+  });
+
+  map.on('load', () => {
+    // Generate a polygon using turf.circle
+    // See https://turfjs.org/docs/#circle
+    const radius = 0.6; // kilometer
+    const options = {
+      steps: 64,
+      units: 'kilometers',
+    };
+    const circle = turf.circle([lng, lat], radius, options);
+
+    // Add the circle as a GeoJSON source
+    map.addSource('location-radius', {
+      type: 'geojson',
+      data: circle,
+    });
+
+    // Add a fill layer with some transparency
+    map.addLayer({
+      id: 'location-radius',
+      type: 'fill',
+      source: 'location-radius',
+      paint: {
+        'fill-color': 'orangered',
+        'fill-opacity': 0.2,
+      },
+    });
+
+    // Add a line layer to draw the circle outline
+    map.addLayer({
+      id: 'location-radius-outline',
+      type: 'line',
+      source: 'location-radius',
+      paint: {
+        'line-color': 'orangered',
+        'line-width': 2,
+      },
+    });
+  });
+  return map;
+}
 ////////////////////////////////////////////////////////////////////////////////
 
 //------------ whereAmI using then() method
@@ -188,14 +245,16 @@ async function whereAmI() {
     const revGeoData = await revGeo.json();
     // console.log(revGeoData);
 
-    // Render current location
+    // Fetch country data
     const currLocation = await fetch(
       `https://restcountries.com/v2/name/${revGeoData.countryName}`
     );
     if (!currLocation.ok) throw new Error('Problem getting country data');
     const [currLocationData] = await currLocation.json();
-    // console.log(currLocationData);
+
+    // Render data
     renderCountry(currLocationData, countryContainer, '', revGeoData);
+    renderMap(lat, lng);
     renderNeighbours(currLocationData.borders);
   } catch (err) {
     containerInputs.style.display = 'none';
@@ -225,13 +284,13 @@ async function searchCountry(countryName) {
 
 //------------ Event Listeners
 btn.addEventListener('click', function () {
-  whereAmI();
   resetUI();
+  whereAmI();
+  if (query800.matches) containerLocation.style.height = '90rem';
 });
-// whereAmI().then(data => console.log(data));
 
 btnSearch.addEventListener('click', function (e) {
   e.preventDefault();
-  searchCountry(inputSearch.value);
   resetUI();
+  searchCountry(inputSearch.value);
 });
